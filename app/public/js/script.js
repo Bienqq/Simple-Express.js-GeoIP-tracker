5463c2e8e8af8220fe97bcfefe3724d0fe637011
Tomasz Bieniek <tomek-1996-1996@o2.pl>
2018-10-07 11:40:43 +0200

    Added some comments

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-    const generateLinkEndpoint = "http://localhost:8090/generate"
+    const generateLinkEndpoint = "http://localhost:8090/generate" // hardcoded to be removed
     new Vue({
         el: "#vue-app",
         data: {
             link: "",
-            linkFromHistory:"<li> Cos</li>"
+            linkFromHistory:"<li> Cos</li>" //for test purposes only
         },
         methods: {
             generate: function () {
